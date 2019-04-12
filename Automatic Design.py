a3455aa82811911b6bb469a46d1343c408d177cc
moqu79 <moqu79@gmail.com>
2019-04-12 20:28:09 +0800

    Update Automatic Design.py

--- Automatic Design.py
+++ Automatic Design.py
@@ -5,6 +5,7 @@ from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont
 from colour import Color
+import openpyxl
 
 title = u'Title of poster'
 subtitle = u'Subtitle'
@@ -21,14 +22,25 @@ x1=x0+buttonWidth
 y0=400
 y1=y0+buttonHeight
 
+
+wb = openpyxl.load_workbook('/Users/moqu/Downloads/素材库1.0/需求/素材需求0410.xlsx')
+sheet = wb['Sheet1']
+
 def watermark():
+    i=2
     for files in glob.glob('/Users/moqu/Downloads/imglib/*.png'):
         chfont = ImageFont.truetype('/Library/Fonts/_H_HelveticaNeue.ttc',72)
         enfont = ImageFont.truetype('/Library/Fonts/_H_HelveticaNeue.ttc',48)
         buttonfont = ImageFont.truetype('/Library/Fonts/_H_HelveticaNeue.ttc',24)
         img = Image.open(files)
 
-        target = Image.new('RGBA', img.size, (0, 0, 0, 0))
+       # target = Image.new('RGBA', img.size, (0, 0, 0, 0))
+
+        result = sheet.cell(row=i, column=6).value
+        result_list = result.split("\n")
+        title = str(result_list[0])
+        subtitle = str(result_list[1])
+        buttonText = str(result_list[2])
 
         draw = ImageDraw.Draw(img)
         draw.text((img.size[0] - 1060, img.size[1] - 416), title, fill=(255, 100, 100), font=chfont)
@@ -52,9 +64,12 @@ def watermark():
         filename = dir + name + '_result.png'
         sleep(1)
         img.save(filename)
+        print (filename+" Done!")
 
-
+        i=i+1
 
 if __name__=='__main__':
     watermark()
+
+
 
